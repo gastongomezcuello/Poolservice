39670c910e9f64195a3111f20ad5024270ab1a84
gastongomezcuello <gastongomezcuello@gmail.com>
2023-01-08 14:08:44 -0300

    footer_added

--- Poolservice/products/views.py
+++ Poolservice/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from products.models import Products
 
 # Create your views here.
+
+def list_products (request):
+    products = Products.objects.all()
+    context = {
+        'productos' : products
+    }
+    return render (request, 'products/products.html', context=context)
