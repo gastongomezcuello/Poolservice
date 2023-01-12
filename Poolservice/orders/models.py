ce899c3e3d39af63f4a3eb295519ce1447f9c6fc
gastongomezcuello <gastongomezcuello@gmail.com>
2023-01-11 23:55:39 -0300

    init_migrate_new_database

--- Poolservice/orders/models.py
+++ Poolservice/orders/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from clients.models import Client
+from products.models import Product
 # Create your models here.
 
 class Order(models.Model):
     
-    CLIENT_CHOICES = (
-
-    )
 
     PAYMENT_CHOICES = (
         ('Efectivo', 'Efectivo'), 
@@ -16,9 +14,9 @@ class Order(models.Model):
         ('Transferencia', 'Transferencia'), 
     )
     
-    client = models.ForeignKey(Client, null=True, blank=True, on_delete=models.CASCADE)
-    product = models.CharField(max_length=100 , verbose_name='Producto') 
-    quantity = models.FloatField(verbose_name='Cantidad')
+    client = models.ForeignKey(Client, null=True, blank=True, on_delete=models.CASCADE, verbose_name='Cliente')
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Producto')
+    quantity = models.FloatField(verbose_name='Cantidad', default=1)
     order_date = models.DateField(verbose_name='Fecha de pedido')
     payment_method = models.CharField(max_length=20, choices=PAYMENT_CHOICES , verbose_name='Forma de pago')
 
